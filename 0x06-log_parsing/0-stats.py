f1ac06736e3c9fb6f227c0d5486ee2c3066ea46f
julgachancipa <julianaga01@gmail.com>
2020-06-02 21:40:45 -0500

    [FIX] - file size

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -19,17 +19,18 @@ if __name__ == '__main__':
         i = 0
         for line in sys.stdin:
             args = line.split()
-            status_line = int(args[-2])
-            file_size = args[-1]
-            sum_file_size += int(file_size)
-            if status_line in status_code:
-                status_code[status_line] += 1
-                i += 1
-                if i % 10 == 0:
-                    print('File size: {}'.format(sum_file_size))
-                    for key, value in sorted(status_code.items()):
-                        if value != 0:
-                            print('{}: {}'.format(key, value))
+            if len(args) > 6:
+                status_line = int(args[-2])
+                file_size = args[-1]
+                sum_file_size += int(file_size)
+                if status_line in status_code:
+                    status_code[status_line] += 1
+                    if i % 10 == 0:
+                        print('File size: {}'.format(sum_file_size))
+                        for key, value in sorted(status_code.items()):
+                            if value != 0:
+                                print('{}: {}'.format(key, value))
+            i += 1
 
         print('File size: {}'.format(sum_file_size))
         for key, value in sorted(status_code.items()):
