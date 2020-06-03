bf3842e27063efd2f21f9669d0f10241d0cd2cac
julgachancipa <julianaga01@gmail.com>
2020-06-02 20:35:05 -0500

    [FIX] - empty file

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -16,29 +16,29 @@ if __name__ == '__main__':
                    500: 0}
 
     try:
-     i = 0
-     for line in sys.stdin:
-         args = line.split()
-         if len(args) == 9:
-             status_line = int(args[-2])
-             file_size = args[-1]
-             status_code[status_line] += 1
-             sum_file_size += int(file_size)
-             i += 1
-             if i % 10 == 0:
-                 print('File size: {}'.format(sum_file_size))
-                 for key, value in sorted(status_code.items()):
-                     if value != 0:
-                         print('{}: {}'.format(key, value))
-    except KeyboardInterrupt:
+        i = 0
+        for line in sys.stdin:
+            args = line.split()
+            if len(args) == 9:
+                status_line = int(args[-2])
+                file_size = args[-1]
+                if status_line in status_code:
+                    status_code[status_line] += 1
+                    sum_file_size += int(file_size)
+                    i += 1
+                if i % 10 == 0:
+                    print('File size: {}'.format(sum_file_size))
+                    for key, value in sorted(status_code.items()):
+                        if value != 0:
+                            print('{}: {}'.format(key, value))
+
         print('File size: {}'.format(sum_file_size))
-        for key, value in status_code.items():
+        for key, value in sorted(status_code.items()):
             if value != 0:
                 print('{}: {}'.format(key, value))
 
-    finally:
-        if i % 10 != 0:
-            print('File size: {}'.format(sum_file_size))
-            for key, value in status_code.items():
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    except KeyboardInterrupt:
+        print('File size: {}'.format(sum_file_size))
+        for key, value in sorted(status_code.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
