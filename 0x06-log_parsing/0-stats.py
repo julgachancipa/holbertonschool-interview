a2d005e222d2beb92079e90c95b6a4743b6be6db
julgachancipa <julianaga01@gmail.com>
2020-06-02 21:53:20 -0500

    [FIX] - dict keys

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,21 +6,21 @@ import sys
 
 if __name__ == '__main__':
     sum_file_size = 0
-    status_code = {200: 0,
-                   301: 0,
-                   400: 0,
-                   401: 0,
-                   403: 0,
-                   404: 0,
-                   405: 0,
-                   500: 0}
+    status_code = {'200': 0,
+                   '301': 0,
+                   '400': 0,
+                   '401': 0,
+                   '403': 0,
+                   '404': 0,
+                   '405': 0,
+                   '500': 0}
 
     try:
         i = 0
         for line in sys.stdin:
             args = line.split()
             if len(args) > 6:
-                status_line = int(args[-2])
+                status_line = args[-2]
                 file_size = args[-1]
                 sum_file_size += int(file_size)
                 if status_line in status_code:
