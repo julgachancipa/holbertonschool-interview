8cfe310e45596de5665d73de93cd1b192b9e0170
julgachancipa <julianaga01@gmail.com>
2020-07-29 14:03:25 -0500

    [FEATURE] - NQueens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -5,14 +5,17 @@ N Queens
 import sys
 
 
-if len(sys.argv) > 2:
+if len(sys.argv) != 2:
     print('Usage: nqueens N')
     exit(1)
-N = sys.argv[-1]
-if not str.isdigit(N):
+N = sys.argv[1]
+try:
+    N = int(N)
+
+except:
     print('N must be a number')
     exit(1)
-N = int(N)
+
 if N < 4:
     print('N must be at least 4')
     exit(1)
